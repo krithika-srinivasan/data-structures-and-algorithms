4e57b844d6c9caf13e5a0c0cbc8a53c5afd13868
Krithika Srinivasan <krithikasrinivasan.0@gmail.com>
2020-01-31 21:48:43 +0530

    Update queue.py

--- queue/queue.py
+++ queue/queue.py
@@ -1,9 +1,10 @@
 class ArrayQueue:
     DEFAULT_QUEUE_SIZE = 10
+
     def __init__(self):
-        self._data = [None] * ArrayQueue.DEFAULT_QUEUE_SIZE
-        self._size = 0
-        self._front = 0
+        self._data = [None] * ArrayQueue.DEFAULT_QUEUE_SIZE  # The size of the queue
+        self._size = 0  # Number of elements in the queue
+        self._front = 0  # Head of the queue
 
     def __len__(self):
         return self._size
@@ -20,15 +21,16 @@ class ArrayQueue:
         if self.is_empty():
             raise Empty('Queue is empty')
         head = self._data[self._front]
-        self._data[self._front] = None
-        self._front = (self._front + 1) % len(self._data)
+        self._data[self._front] = None  # Help with garbage collection
+        self._front = (self._front + 1) % len(self._data)  # Keep moving the head to the right until it wraps around
         self._size -= 1
         return head
 
     def enqueue(self, e):
         if self._size == len(self._data):
             self._resize(2 * len(self._data))
-        back = (self._front + self._size) % len(self._data)
+        back = (self._front + self._size) % len(self._data)  # Insert it at the end of the sequence even if that means
+        # wrapping around 
         self._data[back] = e
         self._size += 1
 
@@ -37,7 +39,7 @@ class ArrayQueue:
         self._data = [None] * cap
         walk = self._front
         for k in range(self._size):
-            self._data[k] = old[walk]
+            self._data[k] = old[walk]  # Reformat the queue so that the head is at index 0 again
             walk = (walk + 1) % len(old)
         self._front = 0
 

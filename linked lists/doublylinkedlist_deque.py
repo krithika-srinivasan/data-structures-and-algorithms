5ac1fd94a046daddf5e21ed0d75322288fb61db8
Krithika Srinivasan <krithikasrinivasan.0@gmail.com>
2020-02-02 16:50:13 +0530

    Add files via upload

---
+++ linked lists/doublylinkedlist_deque.py
@@ -0,0 +1,36 @@
+from doublylinkedlist import _DoublyLinkedList
+
+
+class LinkedDeque(_DoublyLinkedList):
+
+    def first(self):
+        if self.is_empty():
+            raise Empty('Deque is empty')
+        return self._header._next._element
+
+    def last(self):
+        if self.is_empty():
+            raise Empty('Deque is empty')
+        return self._trailer._prev._element
+
+    def insert_first(self, e):
+        self._insert_between(e, self._header, self._header._next)
+
+    def insert_last(self, e):
+        self._insert_between(e, self._trailer._prev, self._trailer)
+
+    def remove_first(self):
+        if self.is_empty():
+            raise Empty('Deque is empty')
+
+        self._delete_node(self._trailer._prev)
+
+    def remove_last(self):
+        if self.is_empty():
+            raise Empty('Deque is empty')
+
+        self._delete_node(self._header._next)
+
+
+class Empty(Exception):
+    pass

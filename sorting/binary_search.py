acb42b1f6bed5959d1454c5c58050e448a46f480
Krithika Srinivasan <krithikasrinivasan.0@gmail.com>
2020-02-08 19:21:37 +0530

    Update binary_search.py

--- sorting/binary_search.py
+++ sorting/binary_search.py
@@ -11,6 +11,22 @@ def binary_search(numbers, value):
             return mid
     return 'Not found'
 
+
+def binary_search_recursive(numbers, value, low, high):
+    while low <= high:
+        mid = (low + high) // 2
+        if numbers[mid] < value:
+            low = mid + 1
+            return (binary_search_recursive(numbers, value, low, high))
+        elif numbers[mid] > value:
+            high = mid - 1
+            return (binary_search_recursive(numbers, value,low, high))
+        else:
+            return mid
+    return 'Not found'
+
+
 numbers = [1,2,3,4,5,6,7,8,9]
 value = 4
 print(binary_search(numbers, value))
+print(binary_search_recursive(numbers, value, 0, 8))
